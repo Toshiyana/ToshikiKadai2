bce55af3694e1064c87e763efa4986a87c580a71
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2021-11-02 00:08:57 +0900

    update by feedback

--- 2/ViewController.swift
+++ 2/ViewController.swift
@@ -8,52 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet private var numTextField1: UITextField!
-    @IBOutlet private var numTextField2: UITextField!
-    @IBOutlet private var resultLabel: UILabel!
-
-    var currentOperation: Operation = .add
+    @IBOutlet private weak var numTextField1: UITextField!
+    @IBOutlet private weak var numTextField2: UITextField!
+    @IBOutlet private weak var resultLabel: UILabel!
+    @IBOutlet private weak var operationSegmentedControl: UISegmentedControl!
 
     enum Operation {
         case add, subtract, multiply, divide
     }
 
-    @IBAction private func didChangeSegment(_ sender: UISegmentedControl) {
-        switch sender.selectedSegmentIndex {
+    var currentOperation: Operation {
+        // 計算時にのみSegmentedControlのoperationは意味を持つので、Action関数を定義する必要ない）
+        switch operationSegmentedControl.selectedSegmentIndex {
         case 0:
-            currentOperation = .add
+            return .add
         case 1:
-            currentOperation = .subtract
+            return .subtract
         case 2:
-            currentOperation = .multiply
+            return .multiply
         case 3:
-            currentOperation = .divide
+            return .divide
         default:
-            break
+            return .add // これはあんまり良くない気がする
         }
     }
 
     @IBAction private func calcButtonPressed(_ sender: UIButton) {
-        if numTextField1.text!.isNumber && numTextField2.text!.isNumber {
-            let num1 = Double(numTextField1.text!)!
-            let num2 = Double(numTextField2.text!)!
-
-            switch currentOperation {
-            case .add:
-                resultLabel.text = String(num1 + num2)
-            case.subtract:
-                resultLabel.text = String(num1 - num2)
-            case .multiply:
-                resultLabel.text = String(num1 * num2)
-            case .divide:
-                if num2 == 0.0 {
-                    resultLabel.text = "割る数には0以外を入力してください"
-                } else {
-                    resultLabel.text = String(num1 / num2)
-                }
-            }
-        } else {
+        // guard文より、num1, num2は数字のみを受け付ける
+        guard let num1 = Double(numTextField1.text ?? "") else {
             resultLabel.text = "数字を入力してください"
+            return
+        }
+        guard let num2 = Double(numTextField2.text ?? "") else {
+            resultLabel.text = "数字を入力してください"
+            return
+        }
+
+        switch currentOperation {
+        case .add:
+            resultLabel.text = String(num1 + num2)
+        case .subtract:
+            resultLabel.text = String(num1 - num2)
+        case .multiply:
+            resultLabel.text = String(num1 * num2)
+        case .divide:
+            if num2 == 0.0 {
+                resultLabel.text = "割る数には0以外を入力してください"
+            } else {
+                resultLabel.text = String(num1 / num2)
+            }
         }
     }
 }
